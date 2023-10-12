39b82633c623ecaed33b0caf4781878fd294fac3
hezekiahkioko <heze9379@gmial.com>
2023-10-12 03:32:47 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - This is the main function.
- * It prints a message and then comments out a loop that would cause an infinite loop.
- *
- * Return: Always returns 0 to indicate successful execution.
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 int main(void)
 {
-    int i;
-    printf("Infinite loop incoming :(\n");
-    i = 0;
-    
-    // This is a commented-out while loop
-    // While loop would cause an infinite loop, so it's avoided.
-    /*
-    while (i < 10)
-    {
-        putchar(i);
-    }
-    */
-    
-    printf("Infinite loop avoided! \\o/\n");
-    return (0);
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
